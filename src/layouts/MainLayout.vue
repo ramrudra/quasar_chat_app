12da6c8af904c6db5ee8e5c962d5461e69a30b33
aman <aman.telkar@gmail.com>
2020-09-04 20:10:40 +0530

    issue fixed working fine app

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,35 +2,37 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
+        <q-btn
+          v-if="$route.fullPath.includes('/chat')"
+          v-go-back.single
+          flat
+          icon="arrow_back"
+          label="Back"
+        />
+
+        <q-toolbar-title class="absolute-center">{{title}}</q-toolbar-title>
 
         <q-btn
-        v-if="$route.fullPath.includes('/chat')"
-        v-go-back.single
-         flat
-         icon="arrow_back"
-         label="Back" />
-
-        <q-toolbar-title class="absolute-center">
-          {{title}}
-        </q-toolbar-title>
+          v-if="!userDetails.userId"
+          to="/auth"
+          class="absolute-right q-pr-sm"
+          no-caps
+          flat
+          icon="account_circle"
+          label="Login"
+        />
 
         <q-btn
-        v-if="!userDetails.userId"
-        to="/auth"
-        class="absolute-right q-pr-sm"
-        no-caps
-        flat
-         icon="account_circle" label="Login" />
-
-         <q-btn
           v-else
           @click="logoutUser"
           class="absolute-right q-pr-sm"
           icon="account_circle"
           no-caps
           flat
-          dense>
-          Logout<br>
+          dense
+        >
+          Logout
+          <br />
           {{ userDetails.name }}
         </q-btn>
       </q-toolbar>
@@ -44,28 +46,27 @@
 <script>
 // import EssentialLink from 'components/EssentialLink.vue'
 
-import { mapState, mapActions } from 'vuex'
+import { mapState, mapActions } from "vuex";
 
 export default {
-
-computed:{
-  ...mapState('store', ['userDetails']),
-  title(){
-    let currentPath = this.$route.fullPath
-    if(currentPath == '/') return 'chatApp'
-    else if(currentPath == '/chat') return 'Chat'
-    else if(currentPath == '/auth') return 'Login'
-
-  }
-},
-    methods: {
-      ...mapActions('store', ['logoutUser'])
-    }
-
-}
+  computed: {
+    ...mapState("store", ["userDetails"]),
+    title() {
+      let currentPath = this.$route.fullPath;
+      if (currentPath == "/") return "chatApp";
+      else if (currentPath == "/chat") return "Chat";
+      else if (currentPath == "/auth") return "Login";
+    },
+  },
+  methods: {
+    ...mapActions("store", ["logoutUser"]),
+  },
+};
 </script>
 <style lang="stylus">
-  .q-toolbar
-    .q-btn
-      line-height: 1.2
+.q-toolbar {
+  .q-btn {
+    line-height: 1.2;
+  }
+}
 </style>
--- src/pages/PageChat.vue
+++ src/pages/PageChat.vue
@@ -2,8 +2,8 @@
   <q-page class="flex column">
     <q-banner class="bg-grey-4 text-center">
       User is offline
-    </q-banner>
-    <div class="q-pa-md column col justify-end ">
+      </q-banner>
+    <div class="q-pa-md column col justify-end">
       <q-chat-message
         v-for="message in messages"
         :key="message.text"
@@ -11,67 +11,56 @@
         :text="[message.text]"
         :sent="message.from == 'me' ? true : false"
       />
-
     </div>
     <q-footer elevated>
-        <q-toolbar >
-          <q-form
-          class="full-width"
-          @submit="sendMessage">
-            <q-input
-             bg-color="white"
-             rounded
-              outlined
-               v-model="newMessage"
-               label="Message"
-               dense>
-            <template v-slot:after>
-            <q-btn
-            round
+      <q-toolbar>
+        <q-form class="full-width" @submit="sendMessage">
+          <q-input
+            bg-color="white"
+            rounded
+            outlined
+            v-model="newMessage"
+            label="Message"
             dense
-            flat
-            type="submit"
-             color="white"
-             icon="send" />
+          >
+            <template v-slot:after>
+              <q-btn round dense flat type="submit" color="white" icon="send" />
             </template>
-            </q-input>
-          </q-form>
-        </q-toolbar>
-      </q-footer>
+          </q-input>
+        </q-form>
+      </q-toolbar>
+    </q-footer>
   </q-page>
 </template>
 
 <script>
+
+import { mapState, mapActions } from 'vuex'
 export default {
-  data(){
-    return{
-      newMessage: '',
-      messages: [
-        {
-          text: 'Hello',
-          from: 'me'
-        },
-         {
-          text: 'Hi',
-          from: 'them'
-        },
-         {
-          text: 'How are you!!!!!',
-          from: 'me'
-        }
-      ]
-   }
+  data() {
+    return {
+      newMessage: ''
+    }
   },
-  methods:{
-    sendMessage(){
+  computed: {
+    ...mapState("store", ["messages"])
+  },
+  methods: {
+    ...mapActions('store', ['firebaseGetMessages','firebaseStopGettingMessages']),
+    
+    sendMessage() {
       this.messages.push({
         text: this.newMessage,
-        from: 'me'
-      })
+        from: "me"
+      });
     }
+  },
+  mounted() {
+    this.firebaseGetMessages(this.$route.params.otherUserId);
+  },
+  destroyed() {
+    this.firebaseStopGettingMessages();
   }
-}
+};
 </script>
-<style>
-
-</style>
+<style></style>
